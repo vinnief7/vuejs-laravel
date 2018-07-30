46a1f23b7d3924e522ae2ffd83a1d2fd51cb0595
vinnie <37674303+vinnief7@users.noreply.github.com>
2018-07-30 14:09:16 -0400

    Add files via upload

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <navbar class="nav navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="" class="navbar-brand">Larticles</a>
+        </div>
+    </navbar>
+</template>
---
+++ storage/framework/views/2a45497a07574568db2c6c1b13705468254a4206.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+        <script>window.laravel = { csrfToken: '<?php echo e(csrf_token()); ?>'}</script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Larticles App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        
+    </head>
+    <body>
+       <div id="app">
+            <nav-bar></nav-bar>
+            <div class="container">
+                <articles></articles>
+            </div>
+       </div>
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+       <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+    </body>
+</html>
